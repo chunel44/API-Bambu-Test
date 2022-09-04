0f22e408fd1a01f692305a0ae9893488d4421295
Estrada44 <jesus@mexiconow.mx>
2022-09-04 02:12:20 -0600

    Create all controller, route and services files

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,2 +1,5 @@
 export * from './auth.controller';
+export * from './card.controller';
+export * from './flight.controller';
+export * from './reservation.controller';
 export * from './user.controller';
